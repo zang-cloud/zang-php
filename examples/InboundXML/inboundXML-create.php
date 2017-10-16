179880d84241dd8fdcee112efb0faf546ba59673
Jaymin Panchal <jpanchal@avaya.com>
2017-10-16 18:17:36 +0530

    Added Gather remaining attriutes

--- examples/InboundXML/inboundXML-create.php
+++ examples/InboundXML/inboundXML-create.php
@@ -6,30 +6,39 @@
  * Time: 22:10
  */
 
+header('Content-Type: application/xml; charset=utf-8');
+
 # First we must import the actual Zang library
 require_once "../../library/ZangApi/InboundXML.php";
 
 # If an error occurs, Zang_Exception will be raised. Due to this,
 # it's a good idea to always do try/catch blocks while querying ZangAPI
-try {
+try
+{
     # Now what we need to do is instantiate the library
     $inboundXml = new Zang_InboundXML();
 
     # Add components to Inbound XML
-    $inboundXml -> dial() -> conference( "ConferenceCall", array(
-        "muted" => "true"
-    ));
-    $inboundXml -> say( "test", array(
+    $inboundXml->gather("", array(
+        "input" => "speech",
+        "method" => "GET",
+        "numDigits" => "4",
+        "finishOnKey" => "#",
+        "timeout" => "24",
+        "hints" => "search",
+        "language" => "en-US",
+        "action" => "http://example.com/example-callback-url/say?example=simple.xml"
+    ))->say("Plese enter 4 digit pin", array(
         "voice" => "male"
-    ));
+    ));;
 
     # If you wish to get back validated Inbound XML as string then use:
     echo $inboundXml;
 
-
-} catch (Exception $e){
+}catch(Exception $e)
+{
     echo $e->getMessage();
     echo "<br><pre>";
-    print_r( $e->getTrace() );
+    print_r($e->getTrace());
     echo "</pre>";
 }
