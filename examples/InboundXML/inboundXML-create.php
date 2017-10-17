102cc99e6b1449086e354d9564a6a741c7236463
Jaymin Panchal <jpanchal@avaya.com>
2017-10-17 11:39:43 +0530

    MMS Added, Changed file permissions

--- examples/InboundXML/inboundXML-create.php
+++ examples/InboundXML/inboundXML-create.php
@@ -19,24 +19,31 @@ try
     $inboundXml = new Zang_InboundXML();
 
     # Add components to Inbound XML
-    $inboundXml->gather("", array(
-        "input" => "speech",
-        "method" => "GET",
-        "numDigits" => "4",
-        "finishOnKey" => "#",
-        "timeout" => "24",
-        "hints" => "search",
-        "language" => "en-US",
-        "action" => "http://example.com/example-callback-url/say?example=simple.xml"
-    ))->say("Plese enter 4 digit pin", array(
-        "voice" => "male"
-    ));;
+    // $inboundXml->gather("", array(
+    //     "input" => "speech",
+    //     "method" => "GET",
+    //     "numDigits" => "4",
+    //     "finishOnKey" => "#",
+    //     "timeout" => "24",
+    //     "hints" => "search",
+    //     "language" => "en-US",
+    //     "action" => "http://example.com/example-callback-url/say?example=simple.xml"
+    // ))->say("Plese enter 4 digit pin", array(
+    //     "voice" => "male"
+    // ));
+
+    
+$inboundXml->mms("This is an MMS sent from Zang",array(
+    "to"=>"+123456",
+    "from"=>"+654321",
+    "mediaUrl"=>"https://tinyurl.com/lpewlmo",
+    "method"=>"GET"
+));
 
     # If you wish to get back validated Inbound XML as string then use:
     echo $inboundXml;
 
-}catch(Exception $e)
-{
+}catch(Exception $e){
     echo $e->getMessage();
     echo "<br><pre>";
     print_r($e->getTrace());
